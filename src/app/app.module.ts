65817d7ba712bd4f1cd0be2e63ee7033a258ec33
Cris <crischus99@gmail.com>
2021-06-13 22:51:30 -0300

    Puse la tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HorariosComponent } from './horarios/horarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorariosComponent
   ],
   imports: [
     BrowserModule
